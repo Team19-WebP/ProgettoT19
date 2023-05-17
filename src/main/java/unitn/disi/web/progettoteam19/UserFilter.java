1019356d33d3930315f09df8e4a5e9d3c3a1330d
Federico Menegoz <menegoz.federico@gmail.com>
2023-05-17 13:32:01 +0200

    userFilter

---
+++ src/main/java/unitn/disi/web/progettoteam19/UserFilter.java
@@ -0,0 +1,31 @@
+package unitn.disi.web.progettoteam19;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebFilter(filterName = "UserFilter")
+public class UserFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpSession session = req.getSession(false);
+        if(session.getAttribute("bannerCookies") == null) {
+            session.setAttribute("provaFilter", "sono una nuova sessione");
+            session.setAttribute("bannerCookies", true);
+        } else {
+            session.setAttribute("provaFilter", "non sono una nuova sessione");
+        }
+//        if (session.getAttribute("auth").equals(false))
+        chain.doFilter(request, response);
+    }
+}
