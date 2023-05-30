6db45c7eeb20b957f9389db57c94f7ddf29db9a2
Gabriele <gitgabriele@gmail.com>
2023-05-30 12:34:52 +0200

    Invio mail check

--- src/main/java/unitn/disi/web/progettoteam19/ServletFormContatti.java
+++ src/main/java/unitn/disi/web/progettoteam19/ServletFormContatti.java
@@ -7,6 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
 @WebServlet(name = "ServletFormContatti", value = "/ServletFormContatti")
 public class ServletFormContatti extends HttpServlet {
     @Override
@@ -16,17 +27,49 @@ public class ServletFormContatti extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-    IOException {
-        /*
-        String nome = request.getParameter("nome");
-        String cognome = request.getParameter("cognome");
-        String email = request.getParameter("email");
-        String comboBox = request.getParameter("comboBox");
-        String altroTextarea = request.getParameter("altroTextarea");
-        */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //simuliamo l'invio della mail
+
+        String to = request.getParameter("email");
+        String from = "tum4world@nessunonoluogonoesiste.com";
+
+        String host = "smtp.fakeserver.com";
+
+
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+
+                return new PasswordAuthentication(from, "19Adm1n!");
+
+            }
+
+        });
+
+        session.setDebug(true);
+
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("Complimenti! Ti ricontatteremo presto!");
+
+            message.setText("Ciao" + request.getParameter("nome") + "! Ti ricontatteremo presto!\n\nLo staff di Tum4World!");
+
+            //Transport.send(message);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+
 
         response.sendRedirect("./invioConfermato.jsp");
     }
--- src/main/webapp/contatti.jsp
+++ src/main/webapp/contatti.jsp
@@ -18,11 +18,6 @@
             chiamandoci allo +39 0461 123456 dal lunedì al venerdì dalle 8:00 alle 17:00 oppure contattaci utilizzando il form
             presente qui sotto, ti risponderemo al più presto!
         </p>
-<%--        <ul>--%>
-<%--            <li><h3>Email: </h3></li>--%>
-<%--            <li><h3>Telefono: +39 0461 123456</h3></li>--%>
-<%--        </ul><br><br>--%>
-<%--        <h2>Per saperne di più, inserisci i tuoi dati:</h2>--%>
     </div>
     <br>
     <div> <%-- TODO metterlo in un css  style="height: 60%"--%>
@@ -46,39 +41,6 @@
         <br><br>
         <div class="inputAndLabel">
             <label for="comboBox" id="comboBoxLabel">Motivo del contatto:</label><br>
-
-        <%-- per utilizzare input radio al posto della comboBox...
-
-        <input type="radio" id="unirsi" name="altroRadio" value="Mi piacerebbe unirmi all'associazione
-        Tum4world." required>
-        <label for="unirsi">Mi piacerebbe unirmi all'associazione Tum4world.</label><br><br>
-        <input type="radio" id="info" name="altroRadio" value="Vorrei saperne di più su quello che fate." required>
-        <label for="info">Vorrei saperne di più su quello che fate.</label><br><br>
-        <input type="radio" id="altro" name="altroRadio" value="Altro: " required>
-        <label for="altroText">Altro: </label><br><br>
-        <textarea id="altroText" name="altroText" maxlength="300" cols="100" rows="3"></textarea>
-        <br><br>
-        <script>
-            let inputUnirsi = document.querySelector("#unirsi");
-            let inputInfo = document.querySelector("#info");
-            let inputAltro = document.querySelector("#altro");
-
-            let textarea = document.querySelector("#altroText");
-
-            textarea.disabled = true;
-
-            inputUnirsi.addEventListener("click", stateHandleAltro);
-            inputInfo.addEventListener("click", stateHandleAltro);
-            inputAltro.addEventListener("click", stateHandleAltro);
-
-            function stateHandleAltro() {
-                if (inputAltro.checked == true) {
-                    textarea.disabled = false;
-                } else {
-                    textarea.disabled = true;
-                }
-            }
-        </script> --%>
 
             <select id="comboBox" name="comboBox">
                 <option value="iscrizione">Mi piacerebbe unirmi alla vostra associazione</option>
